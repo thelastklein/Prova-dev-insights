0e56c31cde67c8e819a413618d0c0d88edd5a27f
Igor Klein <thelastklein@gmail.com>
2022-03-25 15:37:57 -0300

    Criando fetch Post
    rota post para adicionar novos carros na api.

--- prova-dev-igor/script.add.js
+++ prova-dev-igor/script.add.js
@@ -1,25 +1,16 @@
 function novoCarro(){
-    let carModel = document.getElementById('car-model-input').value
-    let carValue = document.getElementById('car-value-input').value
-    let carDesc = document.getElementById('car-desc-input').value
-    let carImg = document.getElementById('car-img-input')
 
+    let formCar = document.getElementById('form-car')
+
+    let formData = new FormData(formCar)
 
     let url = "https://imdev.azurewebsites.net/vendarro/create-carro.php"
 
 
     fetch(url, {
         method: "POST",
-        body: JSON.stringify({
-            "modelo-carro": carModel,
-            "valor-carro": carValue,
-            "description-carro": carDesc,
-            "file-carro": carImg.files[0]
-        }),
-        // headers:{
-        //     "content-Type": "application/json; charset=UTF-8"
-        // }
-     }).then(res => res.json()).then(data => console.log(data))
+        body: formData
+     }).then(res => res.json()).then(data => alert(data))
  
 }
 
